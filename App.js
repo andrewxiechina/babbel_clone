fdaf71da81c7a8c5da803a2cdb8b18a291f3620f
AndyXie <andrewcrab.china@gamil.com>
2017-05-10 16:54:39 -0700

    init

---
+++ App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {
+  StatusBar,
+  Text,
+  View
+} from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      /* Setup status bar to be white and view to extend to whole screen */
+      <View style={{flex: 1, backgroundColor: 'darkgreen'}}>
+        <StatusBar barStyle='light-content' />
+      </View>
+    );
+  }
+}
