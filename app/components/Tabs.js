9dea7497cf940d184d470b512efc99d24069c578
AndyXie <andrewcrab.china@gamil.com>
2017-05-10 18:49:28 -0700

    menu

--- app/components/Tabs.js
+++ app/components/Tabs.js
@@ -6,16 +6,33 @@ import {
 } from 'react-native';
 import Contacts from './Contacts';
 import ScrollableTabView from 'react-native-scrollable-tab-view';
+import { Ionicons } from '@expo/vector-icons';
+import style from './style'
 
 export default class Tabs extends React.Component {
   render() {
     return (
-      /* Setup status bar to be white and view to extend to whole screen */
-      <ScrollableTabView>
-        <Contacts tabLabel="Contacts" />
-        <Contacts tabLabel="Flow" />
-        <Contacts tabLabel="Jest" />
-      </ScrollableTabView>
+      <View style={{flex: 1}}>
+        <View style={[style.bgPrimary, style.tabTop]}>
+          <Text style={[style.h3, style.white, {marginLeft: 15}]}>WhatsApp</Text>
+          <View style={[style.row, {marginRight: 10}]}>
+            <Ionicons name="ios-search" color='#fff' size={23} style={{ padding:10 }} />
+            <Ionicons name="ios-chatbubbles-outline" color='#fff' size={23} style={{ padding:10 }} />
+            <Ionicons name="ios-more" color='#fff' size={23} style={{ padding:10 }} />
+          </View>
+        </View>
+        <ScrollableTabView
+          tabBarUnderlineStyle={{backgroundColor: '#fff', height: 2}}
+          tabBarTextStyle={{fontSize: 16}}
+          tabBarBackgroundColor ='#075e54'
+          tabBarActiveTextColor="#fff"
+          tabBarInactiveTextColor="#88b0ac"
+          >
+          <Contacts tabLabel="Contacts" />
+          <Contacts tabLabel="Flow" />
+          <Contacts tabLabel="Jest" />
+        </ScrollableTabView>
+      </View>
     );
   }
 }
---
+++ app/components/style.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import {
+  StatusBar,
+  Text,
+  View,
+  StyleSheet,
+} from 'react-native';
+
+var style = StyleSheet.create({
+  row: {
+    flexDirection: 'row'
+  },
+  bgPrimary: {
+    backgroundColor:'#075e54',
+  },
+  tabTop: {
+    alignItems:'center',
+    paddingTop:15,
+    justifyContent:'space-between',
+    flexDirection: 'row'
+  },
+  h1: {
+    fontSize: 30
+  },
+  h2: {
+    fontSize: 26
+  },
+  h3: {
+    fontSize: 22
+  },
+  h4: {
+    fontSize: 19
+  },
+  h5: {
+    fontSize: 16
+  },
+  fontNormal: {
+    fontSize: 12
+  },
+  fontSmall: {
+    fontSize: 10
+  },
+  white: {
+    color: 'white'
+  },
+  black: {
+    color: 'black'
+  }
+});
+
+export default style
